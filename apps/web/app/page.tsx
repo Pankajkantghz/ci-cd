adc0df645d5bc3a942b37d6825bf34606fd9e13a
Pankaj Yadav <pankaj.py96810@gmail.com>
2026-03-17 13:20:25 +0530

    added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,7 +7,9 @@ export default async function Home() {
 
   return (
     <div>
+      <h1>name</h1>
       {user?.username}
+       <h1>password</h1>
       {user?.password}
       <br />
       <div>
